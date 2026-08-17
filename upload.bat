9d66ee7ed4c259599e78e7db2f54b85c26a2b814
1-Nekomi <1372718707@qq.com>
2026-08-17 18:33:15 +0800

    更新脚本注释

--- upload.bat
+++ upload.bat
@@ -1,3 +1,8 @@
+REM 开始前请在该目录下配置.env文件
+REM ALGOLIA_APP_ID= YourAlgoliaAppId
+REM ALGOLIA_ADMIN_KEY= YourAlgoliaAdminKey
+REM ALGOLIA_INDEX_NAME= YourAlgoliaIndexName
+REM ALGOLIA_INDEX_FILE= YourAlgoliaIndexFile  
 
 REM npm install -g atomic-algolia
 
--- upload.sh
+++ upload.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Linux/MacOS "chmod +x upload.sh" to make it executable
 
+# 开始前请在该目录下配置 .env 文件
+# ALGOLIA_APP_ID= YourAlgoliaAppId
+# ALGOLIA_ADMIN_KEY= YourAlgoliaAdminKey
+# ALGOLIA_INDEX_NAME= YourAlgoliaIndexName
+# ALGOLIA_INDEX_FILE= YourAlgoliaIndexFile
+
 # npm install -g atomic-algolia
 
 echo "正在构建 Hugo 站点（生成 public 目录）..."
