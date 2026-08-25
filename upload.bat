68be17db5504c113d03bc8b9d843021e195becb2
1-Nekomi <1372718707@qq.com>
2026-08-25 21:27:11 +0800

    config: 更新Windows的Algolia数据上传脚本

--- upload.bat
+++ upload.bat
@@ -1,3 +1,5 @@
+@echo off
+
 REM 开始前请在该目录下配置.env文件
 REM ALGOLIA_APP_ID= YourAlgoliaAppId
 REM ALGOLIA_ADMIN_KEY= YourAlgoliaAdminKey
@@ -6,8 +8,7 @@ REM ALGOLIA_INDEX_FILE= YourAlgoliaIndexFile
 
 REM npm install -g atomic-algolia
 
-echo 正在构建 Hugo 站点（生成 public 目录）...
 hugo
-echo 正在上传数据到 Algolia...
+
 call atomic-algolia
 pause
